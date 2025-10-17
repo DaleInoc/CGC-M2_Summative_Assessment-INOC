ddc2a49a6337f9f36f8d3425f3cb2822b76c050f
DaleInoc <ddInoc@mcm.edu.ph>
2025-10-17 17:48:33 +0800

    added getParticleSystem.js, added fire texture

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "/assets/js/three.module";
 import { GLTFLoader } from "/assets/js/GLTFLoader";
 import {OrbitControls} from "/assets/js/OrbitControls";
+import {getParticleSystem} from "/assets/js/getParticleSystem";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -42,11 +43,17 @@ loader.load(
     }
 );
 
+const fireEffect = new getParticleSystem({
+    camera,
+    emitter: new THREE.Vector3(0,0,0),
+    parent: scene,
+    rate: 30,
+    texture: "assets/textures/fire.png"
+})
 camera.position.z = 5;
 
 function animate() {
-
-
+    fireEffect.update(0.016);
     renderer.render( scene, camera );
 
 }
